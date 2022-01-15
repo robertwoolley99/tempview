3f566d64c48380867e01816629102d6115930682
Robert Woolley <rob@robertwoolley.co.uk>
2022-01-15 19:18:41 +0000

    Can cope with invalid postcodes

--- app/services/post_code_and_weather_service.rb
+++ app/services/post_code_and_weather_service.rb
@@ -3,8 +3,16 @@
 # Class which takes a call from the model and manages postcode checker and weather
 class PostCodeAndWeatherService
   def process(postcode)
-    return 'ERROR:NIL' if postcode.nil?
+    return 'FAIL:NIL' if postcode.nil?
     api_key = ENV['WEATHER_API_KEY']
-    return 'ERROR:NO_API_KEY' if api_key.nil?
+    return 'FAIL:NO_API_KEY' if api_key.nil?
+    check_postcode(postcode)
+  end
+
+  private
+
+  def check_postcode(postcode)
+    checked_postcode = PostCodeCheckerService.new.check(postcode)
+    return 'POSTCODE_NOT_VALID' if checked_postcode == 'NOT_VALID'
   end
 end
--- spec/services/post_code_and_weather_service_spec.rb
+++ spec/services/post_code_and_weather_service_spec.rb
@@ -3,15 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe PostCodeAndWeatherService do
-  context 'when there is a problem' do
+  context 'when there is a problem before we make any calls' do
     it 'throws a fail if a nil is passed' do
       pcaws = described_class.new.process(nil)
-      expect(pcaws).to eq('ERROR:NIL')
+      expect(pcaws).to eq('FAIL:NIL')
     end
+
     it 'throws a fail if there is no API key set' do
       ENV.delete('WEATHER_API_KEY')
       pcaws = described_class.new.process('RH2 8HR')
-      expect(pcaws).to eq('ERROR:NO_API_KEY')
+      expect(pcaws).to eq('FAIL:NO_API_KEY')
+    end
+  end
+
+  context 'when we have an issue with the postcode checker' do
+    let(:pccs) { instance_double PostCodeCheckerService }
+
+    before { ENV['WEATHER_API_KEY'] = 'weather_api_key' }
+
+    it 'returns an error if the postcode does not exist' do
+      allow(PostCodeCheckerService).to receive(:new).and_return(pccs)
+      allow(pccs).to receive(:check).and_return('NOT_VALID')
+      pcaws = described_class.new.process('POSTCODE_NOT_EXISTING')
+      expect(pcaws).to eq('POSTCODE_NOT_VALID')
     end
   end
 end
