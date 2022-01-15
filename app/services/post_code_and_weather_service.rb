c9f983fe0eb394a05dffb4d827c119fa9646a89c
Robert Woolley <rob@robertwoolley.co.uk>
2022-01-15 18:55:41 +0000

    Can gracefully manage no API key

--- app/services/post_code_and_weather_service.rb
+++ app/services/post_code_and_weather_service.rb
@@ -3,6 +3,8 @@
 # Class which takes a call from the model and manages postcode checker and weather
 class PostCodeAndWeatherService
   def process(postcode)
-    return 'NIL' if postcode.nil?
+    return 'ERROR:NIL' if postcode.nil?
+    api_key = ENV['WEATHER_API_KEY']
+    return 'ERROR:NO_API_KEY' if api_key.nil?
   end
 end
--- spec/services/post_code_and_weather_service_spec.rb
+++ spec/services/post_code_and_weather_service_spec.rb
@@ -6,7 +6,12 @@ RSpec.describe PostCodeAndWeatherService do
   context 'when there is a problem' do
     it 'throws a fail if a nil is passed' do
       pcaws = described_class.new.process(nil)
-      expect(pcaws).to eq('NIL')
+      expect(pcaws).to eq('ERROR:NIL')
+    end
+    it 'throws a fail if there is no API key set' do
+      ENV.delete('WEATHER_API_KEY')
+      pcaws = described_class.new.process('RH2 8HR')
+      expect(pcaws).to eq('ERROR:NO_API_KEY')
     end
   end
 end
