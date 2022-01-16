78058304bdf519c1401feb4f94db4ee8d158dd51
Robert Woolley <rob@robertwoolley.co.uk>
2022-01-16 13:20:33 +0000

    Can cope with edge cases in model\

--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -7,9 +7,11 @@ class Temperature < ApplicationRecord
     pcaws_output = PostCodeAndWeatherService.new.process(postcode)
     case pcaws_output[:status]
     when 'invalid_postcode'
-      "Sorry - that doesn't appear to be a valid UK postcode. Please try again."
+      "Sorry - #{postcode} doesn't appear to be a valid UK postcode. Please try again."
     when 'fail_weather'
       "Sorry - we can't get the weather for #{pcaws_output[:postcode]} right now. Please try again later."
+    when 'fail_no_api'
+      "Sorry - we have a fault and can't get the weather for #{pcaws_output[:postcode]} right now. Please try again later."
     end
 
   end
--- spec/models/temperature_spec.rb
+++ spec/models/temperature_spec.rb
@@ -12,13 +12,19 @@ RSpec.describe Temperature do
       it 'returns warning of incorrect postcode ' do
         allow(pcaws_id).to receive(:process).and_return({ status: 'invalid_postcode', postcode: 'BOGUS', temp: nil })
         output = described_class.new.current_temp('BOGUS_POSTCODE')
-        expect(output).to eq("Sorry - that doesn't appear to be a valid UK postcode. Please try again.")
+        expect(output).to eq("Sorry - BOGUS_POSTCODE doesn't appear to be a valid UK postcode. Please try again.")
       end
 
       it 'returns warning when the weather service is down.' do
         allow(pcaws_id).to receive(:process).and_return({ status: 'fail_weather', postcode: 'SW1H 0BD', temp: nil })
         output = described_class.new.current_temp('SW1H0BD')
         expect(output).to eq("Sorry - we can't get the weather for SW1H 0BD right now. Please try again later.")
+      end
+
+      it 'returns an error when api key is not set correctly' do
+        allow(pcaws_id).to receive(:process).and_return({ status: 'fail_no_api', postcode: 'SW1H 0BD', temp: nil })
+        output = described_class.new.current_temp('SW1H0BD')
+        expect(output).to eq("Sorry - we have a fault and can't get the weather for SW1H 0BD right now. Please try again later.")
       end
     end
   end
