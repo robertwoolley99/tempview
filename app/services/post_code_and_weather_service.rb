db36e6addf8f00cd03d4fbf2f50b08076f03136e
Robert Woolley <rob@robertwoolley.co.uk>
2022-01-16 10:35:19 +0000

    Changed response of PCWS class to return a had

--- app/services/post_code_and_weather_service.rb
+++ app/services/post_code_and_weather_service.rb
@@ -3,10 +3,10 @@
 # Class which takes a call from the model and manages postcode checker and weather
 class PostCodeAndWeatherService
   def process(postcode)
-    return 'FAIL:NIL' if postcode.nil?
+    return { status: 'fail_nil_postcode', postcode: postcode, temp: nil } if postcode.nil?
 
     @api_key = ENV['WEATHER_API_KEY']
-    return 'FAIL:NO_API_KEY' if @api_key.nil?
+    return { status: 'fail_no_api', postcode: postcode, temp: nil } if @api_key.nil?
 
     check_postcode(postcode)
   end
@@ -15,7 +15,7 @@ class PostCodeAndWeatherService
 
   def check_postcode(postcode)
     checked_postcode = PostCodeCheckerService.new.check(postcode)
-    return 'ERROR:POSTCODE_NOT_VALID' if checked_postcode == 'NOT_VALID'
+    return { status: 'invalid_postcode', postcode: postcode, temp: nil } if checked_postcode == 'NOT_VALID'
 
     if checked_postcode == 'NOT_CHECKED'
       get_weather(postcode)
@@ -26,7 +26,7 @@ class PostCodeAndWeatherService
 
   def get_weather(postcode)
     weather = WeatherService.new.forecast(@api_key, postcode)
-    return 'FAIL:WEATHER_SERVICE_NOT_AVAILABLE' if weather == 'FAIL'
+    return { status: 'fail_weather', postcode: 'SW1H 0BD', temp: nil } if weather == 'FAIL'
 
     weather
   end
--- app/services/post_code_checker_service.rb
+++ app/services/post_code_checker_service.rb
@@ -3,17 +3,17 @@
 require 'faraday/net_http'
 # Class which calls an API to check postcodes
 class PostCodeCheckerService
-  def check(postcode_input)
-    truncated_string = postcode_input.tr(' ', '')
-    call_postcodes_io(truncated_string)
+  def check(postcode_string)
+    truncated_postcode_string = postcode_string.tr(' ', '')
+    call_postcodes_io(truncated_postcode_string)
   end
 
   private
 
-  def call_postcodes_io(truncated_string)
+  def call_postcodes_io(truncated_postcode_string)
     Faraday.default_adapter = :net_http
     begin
-      response = Faraday.get("#{POST_CODE_CHECK_URL}#{truncated_string}")
+      response = Faraday.get("#{POST_CODE_CHECK_URL}#{truncated_postcode_string}")
     rescue StandardError
       return 'CANNOT_CHECK'
     end
