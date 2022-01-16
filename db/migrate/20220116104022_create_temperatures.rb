2a34a08ef4c9c1bbccfc991d03cfd46468b22aba
Robert Woolley <rob@robertwoolley.co.uk>
2022-01-16 10:41:06 +0000

    Created simple temp model and blank controller

---
+++ db/migrate/20220116104022_create_temperatures.rb
@@ -0,0 +1,10 @@
+class CreateTemperatures < ActiveRecord::Migration[7.0]
+  def change
+    create_table :temperatures do |t|
+      t.float :min_temp
+      t.float :max_temp
+
+      t.timestamps
+    end
+  end
+end
