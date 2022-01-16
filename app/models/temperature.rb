b6734dfebe87ce6d5c8b2ec2ca703d66f8ad4be5
Robert Woolley <rob@robertwoolley.co.uk>
2022-01-16 12:17:12 +0000

    Bogus postcodes working

--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -3,6 +3,7 @@
 # Model to keep temperatures
 class Temperature < ApplicationRecord
   def current_temp(postcode)
-    PostCodeAndWeatherService.new.process(postcode)
+    pcaws_output = PostCodeAndWeatherService.new.process(postcode)
+    return "Sorry - that doesn't appear to be a valid UK postcode. Please try again."
   end
 end
--- spec/models/temperature_spec.rb
+++ spec/models/temperature_spec.rb
@@ -1,18 +1,29 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'temperature.rb'
 
 RSpec.describe Temperature do
   describe 'current_temp method' do
     context 'when working with PCWS' do
-      let(:pcaws) { instance_spy PostCodeAndWeatherService }
+      let(:pcaws_spy) { instance_spy PostCodeAndWeatherService }
 
       it 'calls pcaws correctly' do
-        allow(PostCodeAndWeatherService).to receive(:new).and_return(pcaws)
-        temp = described_class.new
-        temp.current_temp('RH2 8HR')
-        expect(pcaws).to have_received(:process).with('RH2 8HR')
+        allow(PostCodeAndWeatherService).to receive(:new).and_return(pcaws_spy)
+        described_class.new.current_temp('RH2 8HR')
+        expect(pcaws_spy).to have_received(:process).with('RH2 8HR')
+      end
+    end
+
+    context 'when PCAWS returns errors correct text returned' do
+      let(:pcaws_id) { instance_double PostCodeAndWeatherService}
+
+      before { allow(PostCodeAndWeatherService).to receive(:new).and_return(pcaws_id) }
+
+      it 'returns warning of incorrect postcode ' do
+        allow(pcaws_id).to receive(:process).and_return({ status: 'invalid_postcode', postcode: 'BOGUS', temp: nil })
+        output = described_class.new.current_temp('BOGUS_POSTCODE')
+        expect(output).to eq("Sorry - that doesn't appear to be a valid UK postcode. Please try again.")
+
       end
     end
   end
