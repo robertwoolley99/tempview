c22f3f7b4cfdc8c2e636232a42e2f6277fd5e8a9
Robert Woolley <rob@robertwoolley.co.uk>
2022-01-17 20:22:08 +0000

    All tests done and working for temp change

--- spec/integration/temperature/temperature_spec.rb
+++ spec/integration/temperature/temperature_spec.rb
@@ -14,9 +14,9 @@ RSpec.describe 'Temperature Range', type: :feature do
 
     it 'updates max temp correctly' do
       visit root_path
-      fill_in 'temperature_max_temp', with: 30.0
+      fill_in 'temperature_max_temp', with: 40.0
       click_button('Update Maximum and Minimum Temperatures')
-      expect(page).to have_field('temperature_max_temp', with: '30.0')
+      expect(page).to have_field('temperature_max_temp', with: '40.0')
     end
 
     it 'updates min temp correctly' do
@@ -31,6 +31,27 @@ RSpec.describe 'Temperature Range', type: :feature do
       fill_in 'temperature_min_temp', with: -10.0
       click_button('Update Maximum and Minimum Temperatures')
       expect(page).to have_content('Update completed.')
+    end
+
+    it 'rejects a max temp which is not higher than min temp' do
+      visit root_path
+      fill_in 'temperature_max_temp', with: 10
+      click_button('Update Maximum and Minimum Temperatures')
+      expect(page).to have_field('temperature_max_temp', with: '30.0')
+    end
+
+    it 'rejects a min temp which is not lower than max temp' do
+      visit root_path
+      fill_in 'temperature_min_temp', with: 90
+      click_button('Update Maximum and Minimum Temperatures')
+      expect(page).to have_field('temperature_min_temp', with: '20.0')
+    end
+
+    it 'tell us to ensure that the max temp is greater than the min temp' do
+      visit root_path
+      fill_in 'temperature_min_temp', with: 90
+      click_button('Update Maximum and Minimum Temperatures')
+      expect(page).to have_content('The minimum temperature must be less than the maximum temperature. Please try again.')
     end
   end
 end
